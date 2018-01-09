ee2e719d6883c63682a0b29c9e2ea4f031777fbd
Morgam <Moggelicious@hotmail.com>
2018-01-09 16:36:55 +0100

    27

---
+++ app/src/main/java/com/example/morga/pokemonpedia/activities/foo.java
@@ -0,0 +1,8 @@
+package com.example.morga.pokemonpedia.activities;
+
+/**
+ * Created by Morga on 2018-01-09.
+ */
+
+public class foo {
+}
---
+++ app/src/main/java/com/example/morga/pokemonpedia/models/foo2.java
@@ -0,0 +1,8 @@
+package com.example.morga.pokemonpedia.models;
+
+/**
+ * Created by Morga on 2018-01-09.
+ */
+
+public class foo2 {
+}
