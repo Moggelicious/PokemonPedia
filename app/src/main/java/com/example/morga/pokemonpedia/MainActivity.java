fee22a8157150ca29f23431d2b1b5144b2351c33
Mike <peevees@users.noreply.github.com>
2018-01-09 18:17:38 +0100

    now calling api and logging it starting parse of result

--- app/src/main/java/com/example/morga/pokemonpedia/MainActivity.java
+++ app/src/main/java/com/example/morga/pokemonpedia/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.morga.pokemonpedia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.morga.pokemonpedia.services.ApiCallerClient;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Variables
+    String url ="https://pokeapi.co/api/v2/pokemon/?limit=151";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new ApiCallerClient().execute(url);
     }
 }
---
+++ app/src/main/java/com/example/morga/pokemonpedia/services/JSONHandler.java
@@ -0,0 +1,12 @@
+package com.example.morga.pokemonpedia.services;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+import org.json.JSONArray;
+import java.util.ArrayList;
+
+public class JSONHandler {
+
+    private JSONObject json;
+
+}
